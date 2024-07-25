43b2a49b0b9e57768d1e20435f8769a5b668a38b
Sudarraja <demo@gmail.com>
2024-07-25 22:32:51 +0530

    changed

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@
 public class Hello {
     public static void main(String args[]) {
 
-        int a = 100;
+        int a = 200;
         int b = 2;
-        System.out.print(a + b);
+        System.out.print(a * b);
 
     }
 }
