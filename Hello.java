aab9952bcbfa8d127ff5ddd5b8f23b3a635ef1df
Sudarraja <demo@gmail.com>
2024-07-25 22:16:04 +0530

    switched branch

--- Hello.java
+++ Hello.java
@@ -1,10 +1,9 @@
 
 public class Hello {
-    public static void main(String args[])
-    {
-        int a=10;
-        int b=2;
-        System.out.print(a+b);
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 2;
+        System.out.print(a * b);
 
     }
 }
